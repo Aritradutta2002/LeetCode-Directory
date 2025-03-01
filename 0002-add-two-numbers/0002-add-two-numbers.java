ccb0c74478129cb50079ba65dd4884526eae6fe7
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-02 00:16:20 +0530

    Time: 1 ms (100%), Space: 44 MB (98.39%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,47 +10,27 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int num1 = returnValue(l1);
-        int num2 = returnValue(l2);
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
 
-        int num = num1 + num2; // No need to reverse digits
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
 
-        return buildListNode(num);
-    }
-
-    public ListNode buildListNode(int num) {
-        if (num == 0) return new ListNode(0);
-
-        ListNode root = null;
-        ListNode prev = null;
-
-        while (num != 0) {
-            int val = num % 10;
-            ListNode newNode = new ListNode(val);
-
-            if (root == null) {
-                root = newNode; 
-            } else {
-                prev.next = newNode; 
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
             }
 
-            prev = newNode; 
-            num /= 10;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
         }
 
-        return root;
-    }
-
-    int returnValue(ListNode root) {
-        int num = 0;
-        int place = 1;
-
-        while (root != null) {
-            num += root.val * place;
-            root = root.next;
-            place *= 10;
-        }
-
-        return num;
+        return dummyHead.next;
     }
 }
