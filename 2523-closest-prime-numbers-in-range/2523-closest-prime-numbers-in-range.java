b82e11219702598afb0d6efee2183ada33149b81
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-07 19:17:59 +0530

    Time: 85 ms (77.6%), Space: 44.7 MB (75.6%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -1,34 +1,40 @@
 class Solution {
-    public static int[] closestPrimes(int left, int right) {
-        int[] res = {-1, -1};
-        int minDiff = Integer.MAX_VALUE;
-        int prevPrime = -1;
+ public static int[] closestPrimes(int left, int right) {
+    int[] res = {-1, -1};
+    int minDiff = Integer.MAX_VALUE;
+    int prevPrime = -1;
 
-        for (int i = left; i <= right; i++) {
-            if (isPrime(i)) {
-                if (prevPrime != -1) {
-                    int currDiff = i - prevPrime;
-                    if (currDiff < minDiff) {
-                        minDiff = currDiff;
-                        res[0] = prevPrime;
-                        res[1] = i;
-                    }
+    boolean[] isPrime = sieveOfEratosthenes(right);
+
+    for (int i = left; i <= right; i++) {
+        if (isPrime[i]) {
+            if (prevPrime != -1) {
+                int currDiff = i - prevPrime;
+                if (currDiff < minDiff) {
+                    minDiff = currDiff;
+                    res[0] = prevPrime;
+                    res[1] = i;
                 }
-                prevPrime = i;
+            }
+            prevPrime = i;
+        }
+    }
+    return res;
+}
+
+
+private static boolean[] sieveOfEratosthenes(int n) {
+    boolean[] isPrime = new boolean[n + 1];
+    Arrays.fill(isPrime, true);
+    isPrime[0] = isPrime[1] = false; // 0 and 1 are not primes
+
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
             }
         }
-
-        return res;
     }
-    
-    public static boolean isPrime(long n) {
-        if(n < 2) return false;
-        if(n == 2 || n == 3) return true;
-        if(n%2 == 0 || n%3 == 0) return false;
-        long sqrtN = (long)Math.sqrt(n)+1;
-        for(long i = 6L; i <= sqrtN; i += 6) {
-            if(n%(i-1) == 0 || n%(i+1) == 0) return false;
-        }
-        return true;
-    }
+    return isPrime;
+}
 }
