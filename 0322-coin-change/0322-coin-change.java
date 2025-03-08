a822b3ff06762ea24c9f313280bff3b8998d1976
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-09 00:33:29 +0530

    Time: 14 ms (85.41%), Space: 44.3 MB (71.4%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,42 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        if(amount < 1) return 0;
-
-        int [] minCoinsDP = new int [amount + 1];
-        for(int i = 1; i <= amount; i++){
-            minCoinsDP[i] = Integer.MAX_VALUE;
-            for(var coin : coins){
-                if(coin <= i && minCoinsDP[i - coin] != Integer.MAX_VALUE){
-                    minCoinsDP[i] = Math.min(minCoinsDP[i], 1 + minCoinsDP[i - coin]);
+    /* Using Dynamic Programming Bottom Down Approach */
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        if(minCoinsDP[amount] == Integer.MAX_VALUE) return -1;
-
-        return minCoinsDP[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
+
+    // /* Using Dynamic Programming Top-Down Approach */
+    // static HashMap<Integer, Integer> memo = new HashMap<>();
+
+    // public static int dfs(int amount, int[] coins) {
+    //     if (amount == 0) return 0;
+    //     if (memo.containsKey(amount))
+    //         return memo.get(amount);
+
+    //     int res = Integer.MAX_VALUE;
+    //     for (int coin : coins) {
+    //         if (amount - coin >= 0) {
+    //             int result = dfs(amount - coin, coins);
+    //             if (result != Integer.MAX_VALUE) {
+    //                 res = Math.min(res, 1 + result);
+    //             }
+    //         }
+    //     }
+
+    //     memo.put(amount, res);
+    //     return res;
+    // }
+    // public static int coinChangeTop(int[] coins, int amount) {
+    //     int minCoins = dfs(amount, coins);
+    //     return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+    // }
 }
