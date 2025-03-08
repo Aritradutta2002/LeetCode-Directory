b76b1ed1b7a5cfa238fd5f642282aff5ab324dbf
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-09 01:53:11 +0530

    Time: 14 ms (85.38%), Space: 45.1 MB (32.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,42 +1,32 @@
 class Solution {
-    /* Using Dynamic Programming Bottom Down Approach */
-    public static int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int INF = (int) 1e9;
+        int prev[] = new int[amount + 1];
+        int curr[] = new int[amount + 1];
+
+
         for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
+            if (i % coins[0] == 0) {
+                prev[i] = i / coins[0]; 
+            } else {
+                prev[i] = INF; 
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+
+        for (int i = 1; i < n; i++) {
+            for (int target = 0; target <= amount; target++) {
+                int notTake = prev[target];
+                int take = INF;
+                if (coins[i] <= target) {
+                    take = 1 + curr[target - coins[i]];
+                }
+                curr[target] = Math.min(take, notTake); 
+            }
+            System.arraycopy(curr, 0, prev, 0, amount + 1);
+        }
+
+        int ans = prev[amount];
+        return (ans >= INF) ? -1 : ans;
     }
-
-    // /* Using Dynamic Programming Top-Down Approach */
-    // static HashMap<Integer, Integer> memo = new HashMap<>();
-
-    // public static int dfs(int amount, int[] coins) {
-    //     if (amount == 0) return 0;
-    //     if (memo.containsKey(amount))
-    //         return memo.get(amount);
-
-    //     int res = Integer.MAX_VALUE;
-    //     for (int coin : coins) {
-    //         if (amount - coin >= 0) {
-    //             int result = dfs(amount - coin, coins);
-    //             if (result != Integer.MAX_VALUE) {
-    //                 res = Math.min(res, 1 + result);
-    //             }
-    //         }
-    //     }
-
-    //     memo.put(amount, res);
-    //     return res;
-    // }
-    // public static int coinChangeTop(int[] coins, int amount) {
-    //     int minCoins = dfs(amount, coins);
-    //     return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
-    // }
 }
