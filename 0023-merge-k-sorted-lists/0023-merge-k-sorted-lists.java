8dc2c0078586b5d8c909f5717812a4fdd592f9bc
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-09 11:41:43 +0530

    Time: 0 ms (100%), Space: 44.7 MB (27.94%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,23 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i<lists.length;i++){
-            ListNode temp  = lists[i];
-            while(temp !=null){
-                list.add(temp.val);
-                temp = temp.next;
+
+        if(lists.length == 0) return null;
+        if(lists.length == 1) return lists[0];
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        //finding max, min value in the list
+        for(ListNode root : lists){
+            while(root != null){
+                min = Math.min(min, root.val);
+                max = Math.max(max, root.val);
+                root = root.next;
             }
         }
-        Collections.sort(list);
-        
-        ListNode dommy = new ListNode(-1);
-        ListNode temp = dommy;
+        //creating a list of size of all unique value
+        ListNode[] nodes = new ListNode[max-min+1];
 
-        for(int num:list){
-            ListNode newNode = new ListNode(num);
-            temp.next = newNode;
-            temp = temp.next;
+        //list of nodes of same value
+        for( ListNode root : lists){
+            while( root != null){
+                ListNode next = root.next;
+                ListNode sameValNode = nodes[root.val - min];
+                root.next = sameValNode;
+                nodes[root.val - min] = root;
+                root = next;
+            }
         }
-        return dommy.next;
+        
+        ListNode head = new ListNode(0);
+        ListNode cur = head; 
+
+        // Combining the final list
+         for (ListNode root : nodes) {
+            if (root != null) {
+                cur.next = root;
+                while (root.next != null) {
+                    root = root.next;
+                }
+                cur = root;
+            }
+        }
+
+
+        return head.next;
     }
 }
