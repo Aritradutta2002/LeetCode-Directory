2002d211c92298d3fa60421482c9ca927dbb79c9
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-09 11:41:46 +0530

    Time: 1 ms (99.9%), Space: 44.8 MB (27.94%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,52 +8,47 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
 
         if(lists.length == 0) return null;
         if(lists.length == 1) return lists[0];
 
-        int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
-        //finding max, min value in the list
+        int min = Integer.MAX_VALUE;
         for(ListNode root : lists){
             while(root != null){
-                min = Math.min(min, root.val);
                 max = Math.max(max, root.val);
+                min = Math.min(min, root.val);
                 root = root.next;
             }
         }
-        //creating a list of size of all unique value
-        ListNode[] nodes = new ListNode[max-min+1];
 
-        //list of nodes of same value
-        for( ListNode root : lists){
+        ListNode[] node = new ListNode[max - min + 1];
+
+        for(var root : lists){
             while( root != null){
                 ListNode next = root.next;
-                ListNode sameValNode = nodes[root.val - min];
-                root.next = sameValNode;
-                nodes[root.val - min] = root;
+                ListNode sameValue = node[root.val - min];
+                root.next = sameValue;
+                node[root.val - min] = root;
                 root = next;
             }
         }
-        
-        ListNode head = new ListNode(0);
-        ListNode cur = head; 
 
-        // Combining the final list
-         for (ListNode root : nodes) {
-            if (root != null) {
-                cur.next = root;
-                while (root.next != null) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+
+        for(var root : node){
+            if(root != null){
+                curr.next = root;
+                while(root.next != null){
                     root = root.next;
                 }
-                cur = root;
+                curr = root;
             }
         }
-
-
         return head.next;
     }
 }
