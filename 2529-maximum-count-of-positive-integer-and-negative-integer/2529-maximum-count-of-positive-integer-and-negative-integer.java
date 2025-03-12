3d8ad2179b92544647e439af429a643108676a40
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-12 10:29:43 +0530

    Time: 0 ms (100%), Space: 44.9 MB (79.99%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,24 +1,43 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int negCount = binarySearch(nums, 0);
-        int posCount = nums.length - binarySearch(nums, 1);
-
-        return Math.max(negCount, posCount);
+        int lastnegIndex= lastNegative(nums) +1 ;
+        int firtposIndex = nums.length - firstPositive(nums);
+        return Math.max(lastnegIndex , firtposIndex);
     }
 
-    private int binarySearch(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, result = nums.length;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                result = mid;
-                right = mid - 1;
+    public int lastNegative(int[] nums){
+        int start = 0 ;
+        int end = nums.length-1 ;
+        int ans = -1 ;
+
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid]<0){
+                ans = mid ;
+                start = mid+1 ;
+            }else{
+                end = mid-1 ;
             }
         }
-        
-        return result;
+
+        return ans ;
+    }
+
+    public int firstPositive(int[] nums){
+        int start = 0 ;
+        int end = nums.length-1;
+        int ans = nums.length;
+
+        while(start<=end){
+            int mid = (start + end )/2;
+            if(nums[mid]>0){
+                ans=mid;
+                end=mid-1;
+            }else{
+                start = mid+1 ;
+            }
+        }
+
+        return ans ;
     }
 }
