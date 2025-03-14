5c7154e99af523f9866401d226412dfbf66b88cd
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-14 23:44:26 +0530

    Time: 3 ms (64.38%), Space: 10.6 MB (36.04%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<int>> placements = NQueensInt(n);
-        return convertToBoard(n, placements);
-    }
-
-    bool IsValid(const vector<int> &col_placement) {
-    int row_id = col_placement.size() - 1;
-    for (int i = 0; i < row_id; ++i) {
-        int diff = abs(col_placement[i] - col_placement[row_id]);
-        if (diff == 0 || diff == row_id - i) {
-            return false;
+    vector<vector<string>>ans;
+    int N;
+    bool isSafe(vector<string>&board,int row,int col){
+        for(int i=row;i>=0;i--){
+            if(board[i][col]=='Q') return false;
         }
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        for(int i=row,j=col;i>=0 && j<N;i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
     }
-    return true;
-}
-
-void solveNQueen(int n, int row, vector<int> &col_placement, vector<vector<int>> &result) {
-    if (row == n) {
-        result.push_back(col_placement);
-    } else {
-        for (int col = 0; col < n; ++col) {
-            col_placement.push_back(col);
-            if (IsValid(col_placement)) {
-                solveNQueen(n, row + 1, col_placement, result);
+    void solve(vector<string>&board,int row){
+        if(row==N){
+            ans.push_back(board);
+            return;
+        }
+        for(int col=0;col<N;col++){
+            if(isSafe(board,row,col)){
+                board[row][col]='Q';
+                solve(board,row+1);
+                board[row][col]='.';
             }
-            col_placement.pop_back();
         }
     }
-}
-
-vector<vector<int>> NQueensInt(int n) {
-    vector<vector<int>> result;
-    vector<int> col_placement;
-    solveNQueen(n, 0, col_placement, result);
-    return result;
-}
-
-vector<vector<string>> convertToBoard(int n, const vector<vector<int>> &placements) {
-    vector<vector<string>> boardSolutions;
-    for (auto &solution : placements) {
-        vector<string> board;
-        for (int i = 0; i < n; i++) {
-            string row(n, '.');
-            row[solution[i]] = 'Q';
-            board.push_back(row);
-        }
-        boardSolutions.push_back(board);
+    vector<vector<string>> solveNQueens(int n) {
+        if(n==0) return {};
+        N=n;
+        vector<string>board(n,string(n,'.'));
+        solve(board,0);
+        return ans;
     }
-    return boardSolutions;
-}
 };
