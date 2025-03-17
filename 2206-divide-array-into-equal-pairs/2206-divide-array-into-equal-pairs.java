45acfdc82b581bfda0a35e2de398cf5178fc220c
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-17 09:46:30 +0530

    Time: 2 ms (82.79%), Space: 44.2 MB (85.16%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -2,12 +2,13 @@ class Solution {
     public boolean divideArray(int[] nums) {
         long[] arr = new long[1501];
         arr[0] = 0;
-        for(int i = 0; i < nums.length; i++){
-            arr[nums[i]]++;
+        
+        for(var ele : nums){
+            arr[ele]++;
         }
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] % 2 != 0){
+        for(var ele : arr){
+            if(ele % 2 != 0){
                 return false;
             }
         }
