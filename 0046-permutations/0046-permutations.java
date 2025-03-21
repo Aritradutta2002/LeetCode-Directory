1d8f392ce3292744abd26861ce57fc7eff3dbb36
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-21 09:53:15 +0530

    Time: 1 ms (92.8%), Space: 44.7 MB (31.41%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,30 @@
 class Solution {
-    public static List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        backtrack(resultList, new ArrayList<>(), nums);
-        return resultList;
+     public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        permuteRecursion(0, nums, ans);
+        return ans;
     }
 
-    private static void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums) {
-        if (tempList.size() == nums.length) {
-            resultList.add(new ArrayList<>(tempList));
+    private static void permuteRecursion(int index, int[] nums, List<List<Integer>> ans) {
+        if(index == nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++){
+                list.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(list));
             return;
         }
 
-        for (int number : nums) {
-            if (tempList.contains(number)) {
-                continue;
-            }
-
-            tempList.add(number);
-
-            backtrack(resultList, tempList, nums);
-
-            tempList.remove(tempList.size() - 1);
+        for(int i = index; i < nums.length; i++){
+            swap(i, index, nums);
+            permuteRecursion(index + 1, nums, ans);
+            swap(i, index, nums);
         }
+    }
+
+    private static void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
