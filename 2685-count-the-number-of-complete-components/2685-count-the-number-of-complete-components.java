43ea2eb7f64d1f5acf946deab94c5083d2e3dbf4
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-23 01:03:38 +0530

    8 ms (90.92%) 45.7 MB (14.21%) 03-23-2025 2685-count-the-number-of-complete-components Medium .java

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,37 +1,62 @@
 class Solution {
-    private void dfs(int i, Map<Integer, List<Integer>> adj, boolean[] visited, int[] info) {
-        visited[i] = true;
-        info[0]++;
-        info[1] += adj.getOrDefault(i, new ArrayList<>()).size();
-        
-        for (int ngbr : adj.getOrDefault(i, new ArrayList<>())) {
-            if (!visited[ngbr]) {
-                dfs(ngbr, adj, visited, info);
+    class DSU {
+        int[] parent, size;
+
+        DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        int find(int x) {
+            if (parent[x] == x)
+                return x;
+            return parent[x] = find(parent[x]);
+        }
+
+        void union(int x, int y) {
+            int xRoot = find(x), yRoot = find(y);
+            if (xRoot == yRoot)
+                return;
+
+            if (size[xRoot] > size[yRoot]) {
+                parent[yRoot] = xRoot;
+                size[xRoot] += size[yRoot];
+            } else {
+                parent[xRoot] = yRoot;
+                size[yRoot] += size[xRoot];
             }
         }
     }
-    
+
     public int countCompleteComponents(int n, int[][] edges) {
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        int result = 0;
-        
+        DSU dsu = new DSU(n);
+        Map<Integer, Integer> edgeCount = new HashMap<>();
+
         for (int[] edge : edges) {
-            adj.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-            adj.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+            dsu.union(edge[0], edge[1]);
         }
-        
-        boolean[] visited = new boolean[n];
+
+        for (int[] edge : edges) {
+            int root = dsu.find(edge[0]);
+            edgeCount.put(root, edgeCount.getOrDefault(root, 0) + 1);
+        }
+
+        int result = 0;
         for (int i = 0; i < n; i++) {
-            if (visited[i]) continue;
-            
-            int[] info = new int[2];
-            dfs(i, adj, visited, info);
-            
-            if (info[0] * (info[0] - 1) == info[1]) {
-                result++;
+            if (dsu.find(i) == i) {
+                int v = dsu.size[i];
+                int e = edgeCount.getOrDefault(i, 0);
+
+                if ((v * (v - 1)) / 2 == e) {
+                    result++;
+                }
             }
         }
-        
+
         return result;
     }
 }
