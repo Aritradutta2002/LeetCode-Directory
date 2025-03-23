6d122937c1f33d7d3bdbb755938016b75bbb8123
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-24 01:18:59 +0530

    5 ms (48.14%) 45.9 MB (98.01%) 03-24-2025 0977-squares-of-a-sorted-array Easy .java

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,53 +1,55 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i = 0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             nums[i] = (int) nums[i] * nums[i];
         }
         merge(nums);
         return nums;
     }
-    public static void merge(int[] nums){
-        if(nums.length<2){
+
+    public static void merge(int[] nums) {
+        if (nums.length < 2) {
             return;
         }
-        int mid = nums.length/2;
+        int mid = nums.length / 2;
         int[] leftarr = new int[mid];
         int[] rightarr = new int[nums.length - mid];
-        for(int i = 0;i<mid;i++){
+        for (int i = 0; i < mid; i++) {
             leftarr[i] = nums[i];
         }
-        for(int j = mid; j<nums.length;j++){
+        for (int j = mid; j < nums.length; j++) {
             rightarr[j - mid] = nums[j];
         }
         merge(leftarr);
         merge(rightarr);
-        mergearr(leftarr,rightarr,nums);
+        mergearr(leftarr, rightarr, nums);
     }
-    public static void mergearr(int[] leftarr,int[] rightarr, int[] nums){
+
+    public static void mergearr(int[] leftarr, int[] rightarr, int[] nums) {
         int i = 0;
         int j = 0;
         int k = 0;
 
-        while(i<leftarr.length && j<rightarr.length){
-            if(leftarr[i]<rightarr[j]){
+        while (i < leftarr.length && j < rightarr.length) {
+            if (leftarr[i] < rightarr[j]) {
                 nums[k] = leftarr[i];
                 i++;
                 k++;
-            }else{
+            } else {
                 nums[k] = rightarr[j];
                 j++;
                 k++;
             }
         }
-        while(i<leftarr.length){
+        while (i < leftarr.length) {
             nums[k] = leftarr[i];
             i++;
             k++;
         }
-        while(j<rightarr.length){
+        while (j < rightarr.length) {
             nums[k] = rightarr[j];
             j++;
             k++;
         }
     }
-    }
+}
