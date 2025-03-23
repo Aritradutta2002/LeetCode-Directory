f3642b871f5d98a592a9f0174c95a3fdeb87f806
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-24 01:18:35 +0530

    5 ms (48.14%) 46 MB (97.26%) 03-24-2025 0977-squares-of-a-sorted-array Easy .java

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,10 +1,53 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            nums[i] = nums[i] * nums[i];
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = (int) nums[i] * nums[i];
         }
-        Arrays.sort(nums);
+        merge(nums);
         return nums;
     }
-}
+    public static void merge(int[] nums){
+        if(nums.length<2){
+            return;
+        }
+        int mid = nums.length/2;
+        int[] leftarr = new int[mid];
+        int[] rightarr = new int[nums.length - mid];
+        for(int i = 0;i<mid;i++){
+            leftarr[i] = nums[i];
+        }
+        for(int j = mid; j<nums.length;j++){
+            rightarr[j - mid] = nums[j];
+        }
+        merge(leftarr);
+        merge(rightarr);
+        mergearr(leftarr,rightarr,nums);
+    }
+    public static void mergearr(int[] leftarr,int[] rightarr, int[] nums){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i<leftarr.length && j<rightarr.length){
+            if(leftarr[i]<rightarr[j]){
+                nums[k] = leftarr[i];
+                i++;
+                k++;
+            }else{
+                nums[k] = rightarr[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<leftarr.length){
+            nums[k] = leftarr[i];
+            i++;
+            k++;
+        }
+        while(j<rightarr.length){
+            nums[k] = rightarr[j];
+            j++;
+            k++;
+        }
+    }
+    }
