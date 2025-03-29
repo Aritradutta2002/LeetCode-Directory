c6aff200e95768cfc3a06f4f004c215a3a9c199e
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-30 00:46:29 +0530

    407 ms (42.86%) 60.4 MB (52.38%) 03-30-2025 2818-apply-operations-to-maximize-score Hard .java

--- 2818-apply-operations-to-maximize-score/2818-apply-operations-to-maximize-score.java
+++ 2818-apply-operations-to-maximize-score/2818-apply-operations-to-maximize-score.java
@@ -1,29 +1,91 @@
 import java.util.*;
 
 class Solution {
-    public int maxFrequencyScore(int[] nums, long k) {
-        Arrays.sort(nums);
-        int n = nums.length, left = 0, right = n;
-        long[] prefix = new long[n + 1];
-        
-        for (int i = 1; i <= n; i++) prefix[i] = prefix[i - 1] + nums[i - 1];
-        
-        while (left < right) {
-            int mid = (left + right + 1) >> 1;
-            boolean possible = false;
-            
-            for (int i = 0; i <= n - mid; i++) {
-                int j = i + mid, target = nums[(i + j) / 2];
-                long cost = ((i + j) / 2 - i) * (long) target - (prefix[(i + j) / 2] - prefix[i])
-                          + (prefix[j] - prefix[(i + j) / 2]) - ((j - (i + j) / 2) * (long) target);
-                if (cost <= k) {
-                    possible = true;
-                    break;
-                }
+
+    private static final int MOD = 1_000_000_007;
+
+    public int maximumScore(List<Integer> nums, int k) {
+        int n = nums.size();
+        int[] primeScores = new int[n];
+
+        int maxElement = Collections.max(nums);
+        List<Integer> primes = getPrimes(maxElement);
+
+        for (int index = 0; index < n; index++) {
+            int num = nums.get(index);
+            for (int prime : primes) {
+                if (prime * prime > num) break;
+                if (num % prime != 0) continue;
+                primeScores[index]++;
+                while (num % prime == 0) num /= prime;
             }
-            if (possible) left = mid;
-            else right = mid - 1;
+            if (num > 1) primeScores[index]++;
         }
-        return left;
+
+        int[] nextDominant = new int[n];
+        int[] prevDominant = new int[n];
+        Arrays.fill(nextDominant, n);
+        Arrays.fill(prevDominant, -1);
+
+        Stack<Integer> stack = new Stack<>();
+        for (int index = 0; index < n; index++) {
+            while (!stack.isEmpty() && primeScores[stack.peek()] < primeScores[index]) {
+                int topIndex = stack.pop();
+                nextDominant[topIndex] = index;
+            }
+            if (!stack.isEmpty()) prevDominant[index] = stack.peek();
+            stack.push(index);
+        }
+
+        long[] numOfSubarrays = new long[n];
+        for (int index = 0; index < n; index++) {
+            numOfSubarrays[index] = (long) (nextDominant[index] - index) * (index - prevDominant[index]);
+        }
+
+        List<int[]> sortedArray = new ArrayList<>();
+        for (int index = 0; index < n; index++) {
+            sortedArray.add(new int[]{nums.get(index), index});
+        }
+        sortedArray.sort((a, b) -> Integer.compare(b[0], a[0]));
+
+        long score = 1;
+        int processingIndex = 0;
+
+        while (k > 0) {
+            int[] element = sortedArray.get(processingIndex++);
+            int num = element[0];
+            int index = element[1];
+
+            long operations = Math.min(k, numOfSubarrays[index]);
+            score = (score * power(num, operations)) % MOD;
+            k -= operations;
+        }
+
+        return (int) score;
+    }
+
+    private long power(long base, long exponent) {
+        long res = 1;
+        while (exponent > 0) {
+            if (exponent % 2 == 1) res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exponent /= 2;
+        }
+        return res;
+    }
+
+    private List<Integer> getPrimes(int limit) {
+        boolean[] isPrime = new boolean[limit + 1];
+        Arrays.fill(isPrime, true);
+        List<Integer> primes = new ArrayList<>();
+
+        for (int number = 2; number <= limit; number++) {
+            if (!isPrime[number]) continue;
+            primes.add(number);
+            for (long multiple = (long) number * number; multiple <= limit; multiple += number) {
+                isPrime[(int) multiple] = false;
+            }
+        }
+        return primes;
     }
 }
