cd2506ac6f938e62f1172bb11d157d55325abde4
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-03-31 01:40:01 +0530

    4 ms (92.9%) 42.3 MB (60.85%) 03-31-2025 0763-partition-labels Medium .java

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -2,22 +2,22 @@ class Solution {
     public List<Integer> partitionLabels(String s) {
         int n = s.length();
         List<Integer> result = new ArrayList<>();
-    
+
         int[] lastIndex = new int[26];
         for (int i = 0; i < n; i++) {
             lastIndex[s.charAt(i) - 'a'] = i;
         }
         
         int i = 0;
+        int start = 0;
+        int end = 0;
         while (i < n) {
-            int end = lastIndex[s.charAt(i) - 'a'];
-            int j = i;
-            while (j < end) {
-                end = Math.max(end, lastIndex[s.charAt(j) - 'a']);
-                j++;
+            end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+            if (i == end) {
+                result.add(end - start + 1);
+                start = end + 1;
             }
-            result.add(j - i + 1);
-            i = j + 1;
+            i++;
         }
         
         return result;
