1b9336ca2d6d8b71758f09f728729b019e633b83
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-04-04 00:13:54 +0530

    2 ms (97.85%) 57.1 MB (87.1%) 04-04-2025 2874-maximum-value-of-an-ordered-triplet-ii Medium .java

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.java
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.java
@@ -1,13 +1,28 @@
 class Solution {
-
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
-        long res = 0, imax = 0, dmax = 0;
-        for (int k = 0; k < n; k++) {
-            res = Math.max(res, dmax * nums[k]);
-            dmax = Math.max(dmax, imax - nums[k]);
-            imax = Math.max(imax, nums[k]);
+        
+        long maxTriplet = 0;
+        
+        int[] maxRight = new int[n];
+        maxRight[n - 1] = 0; 
+        for (int j = n - 2; j >= 0; j--) {
+            maxRight[j] = Math.max(maxRight[j + 1], nums[j + 1]);
         }
-        return res;
+        
+        int maxI = nums[0];
+        for (int j = 1; j < n - 1; j++) {
+            long currentDiff = maxI - nums[j];
+            long currentProduct = currentDiff * maxRight[j];
+            if (currentProduct > maxTriplet) {
+                maxTriplet = currentProduct;
+            }
+           
+            if (nums[j] > maxI) {
+                maxI = nums[j];
+            }
+        }
+        
+        return maxTriplet;
     }
 }
