b125f3e7ca61e15cf938c852f10f8488e2798ad4
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-04-06 02:31:49 +0530

    0 ms (100%) 41.2 MB (45.75%) 04-06-2025 1863-sum-of-all-subset-xor-totals Easy .java

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,17 +1,8 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int n = nums.length;
-        return solveBacktrack(nums, 0, 0);
-    }
-
-    private int solveBacktrack(int[] nums, int index, int sum) {
-        if (index >= nums.length) {
-            return sum;
-        }
-
-        int pickingElementXor = solveBacktrack(nums, index + 1, sum ^ nums[index]);
-        int NotpickingElementXor = solveBacktrack(nums, index + 1, sum);
-
-        return (pickingElementXor + NotpickingElementXor);
+        int result = 0;
+        for (int num : nums) result |= num;
+        
+        return result << (nums.length - 1);
     }
 }
