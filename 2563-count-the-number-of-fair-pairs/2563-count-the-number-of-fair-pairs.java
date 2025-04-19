326337539940ab2c77c0dd4137589dae7b41699a
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-04-20 00:59:06 +0530

    23 ms (100%) 57.6 MB (21.34%) 04-20-2025 2563-count-the-number-of-fair-pairs Medium .java

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,14 +1,20 @@
 class Solution {
-    public long countFairPairs(int[] nums, int lower, int upper) {
-        
-        int n = nums.length;
+     public static long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        long countUpper = countPairsAtMost(nums, upper);
+        long countLower = countPairsAtMost(nums, lower - 1);
+        return countUpper - countLower;
+    }
+    
+    private static long countPairsAtMost(int[] nums, int x) {
         long count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                int sum = nums[i] + nums[j];
-                if(sum >= lower && sum <= upper){
-                    count++;
-                }
+        int i = 0, j = nums.length - 1;
+        while(i < j) {
+            if(nums[i] + nums[j] <= x) {
+                count += j - i;
+                i++;
+            } else {
+                j--;
             }
         }
         return count;
