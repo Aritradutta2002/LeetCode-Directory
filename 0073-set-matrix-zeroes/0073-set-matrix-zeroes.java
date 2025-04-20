2248c5c7924b1c39a47d6e66047f006ba37074dc
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-04-21 00:02:15 +0530

    1 ms (74.14%) 45.6 MB (66.99%) 04-21-2025 0073-set-matrix-zeroes Medium .java

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,26 @@
+class Solution {
+   public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] rows = new int[n];
+        int[] cols = new int[m];
+        Arrays.fill(rows, 0);
+        Arrays.fill(cols, 0);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(rows[i] == 1 || cols[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
