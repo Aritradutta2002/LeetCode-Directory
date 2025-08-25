0c31bd3605566b5f8f530fe2c6c3152b2f9acf79
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2025-08-25 10:51:09 +0530

    3 ms (61.54%) 47.2 MB (70.4%) 08-25-2025 0498-diagonal-traverse Medium .java

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -1,33 +1,56 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
+        // If the grid is empty, just return an empty answer.
+        if (mat == null || mat.length == 0) {
+            return new int[0];
+        }
+
+        // Get the height (number of rows) and width (number of columns) of the grid.
+        int m = mat.length;
+        int n = mat[0].length;
+
+        // This is our final answer list. It needs to be big enough to hold every number.
         int[] result = new int[m * n];
-        int index = 0;
         
-        for (int d = 0; d < m + n - 1; d++) {
-            
-            if (d % 2 == 0) {
-                int row = (d < m) ? d : m - 1;
-                int col = (d < m) ? 0 : d - m + 1;
-                
-                while (row >= 0 && col < n) {
-                    result[index++] = mat[row][col];
-                    row--;  
-                    col++;  
-                }
-            } else {
-                
-                int col = (d < n) ? d : n - 1;
-                int row = (d < n) ? 0 : d - n + 1;
-                
-                while (row < m && col >= 0) {
-                    result[index++] = mat[row][col];
+        // We'll start our walk at the top-left corner, which is row 0, column 0.
+        int row = 0, col = 0;
+
+        // We need to loop enough times to visit every single number in the grid.
+        for (int i = 0; i < m * n; i++) {
+            // First, add the number at our current spot to our answer list.
+            result[i] = mat[row][col];
+
+            // Now, we need to figure out where to walk next.
+
+            // If (row + col) is an EVEN number, we are moving UP and RIGHT.
+            if ((row + col) % 2 == 0) { 
+                // If we hit the RIGHT wall... we must move DOWN one step.
+                if (col == n - 1) {
                     row++;
-                    col--;  
+                // If we hit the TOP wall... we must move RIGHT one step.
+                } else if (row == 0) {
+                    col++;
+                // If we are not at a wall, just move diagonally UP and RIGHT.
+                } else {
+                    row--;
+                    col++;
+                }
+            } else { // If (row + col) is an ODD number, we are moving DOWN and LEFT.
+                // If we hit the BOTTOM wall... we must move RIGHT one step.
+                if (row == m - 1) {
+                    col++;
+                // If we hit the LEFT wall... we must move DOWN one step.
+                } else if (col == 0) {
+                    row++;
+                // If we are not at a wall, just move diagonally DOWN and LEFT.
+                } else {
+                    row++;
+                    col--;
                 }
             }
         }
         
+        // Once the loop is done, we have visited all the numbers in the right order!
         return result;
     }
 }
