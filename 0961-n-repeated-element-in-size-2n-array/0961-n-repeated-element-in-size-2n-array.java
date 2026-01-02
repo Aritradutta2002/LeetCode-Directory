4534ec05b11fc5256b2f7df855d08dd2ac5c98cd
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-01-02 22:42:24 +0530

    17 ms (29.06%) 47.2 MB (75.13%) 01-02-2026 0961-n-repeated-element-in-size-2n-array Easy .java

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -4,19 +4,19 @@ class Solution {
 
         int len = nums.length;
         int n = (len / 2);
-        int pointer = nums[0];
-        int count = 0;
-        Arrays.sort(nums);
-        for (int i = 0; i < len; i++) {
-            if (pointer != nums[i]) {
-                pointer = nums[i];
-                count = 0;
-            }
-            count++;
-            if(count == n){
-                return pointer;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (var element : nums) {
+            map.put(element, map.getOrDefault(element, 0) + 1);
+        }
+
+        for (int k : map.keySet()) {
+            if (map.get(k) > 1) {
+                return k;
             }
         }
+
         return -1;
     }
 }
