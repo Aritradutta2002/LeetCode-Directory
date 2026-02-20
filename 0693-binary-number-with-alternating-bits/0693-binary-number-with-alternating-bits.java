dda851cc866f7c705925b2d277f3bf679824f80c
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-02-20 10:25:12 +0530

    0 ms (100%) 42.2 MB (63.09%) 02-20-2026 0693-binary-number-with-alternating-bits Easy .java

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        int len = 32 - Integer.numberOfLeadingZeros(n);;
+        int len = Integer.toBinaryString(n).length();
         for(int i = 1; i < len; i++){
             int prevBit = (n >> (i - 1)) & 1;
             int currBit = (n >> (i)) & 1;
