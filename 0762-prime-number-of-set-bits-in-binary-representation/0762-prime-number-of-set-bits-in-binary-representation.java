dfd64f8892bcd6e33a15ef7f54372283079f51a6
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-02-21 15:18:45 +0530

    5 ms (64.72%) 42.2 MB (90.65%) 02-21-2026 0762-prime-number-of-set-bits-in-binary-representation Easy .java

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.java
@@ -12,20 +12,16 @@ class Solution {
     }
 
     public static boolean isPrime(int n) {
-        if (n < 2) {
+        if (n < 2)
             return false;
-        }
-
-        if(n == 2) return true;
-
-        if(n % 2 == 0) return false;
-
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
+        if (n == 2)
+            return true;
+        if (n % 2 == 0)
+            return false; // Handle even numbers
+        for (int i = 3; i * i <= n; i += 2) { // Check only odd divisors
+            if (n % i == 0)
                 return false;
-            }
         }
-
         return true;
     }
 }
