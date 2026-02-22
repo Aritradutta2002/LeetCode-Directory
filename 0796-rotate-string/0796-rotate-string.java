dd2497f19e0cfd209111fe800694c8d91c214888
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-02-22 10:19:28 +0530

    0 ms (100%) 42.9 MB (41.51%) 02-22-2026 0796-rotate-string Easy .java

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+        if(s.length()  != goal.length()){
+            return false;
+        }
+        
+        String search = s.concat(s);
 
-        if(s.length() != goal.length()) return false;
-       
-       String str = s + s;
-
-       return str.contains(goal);
+        return search.contains(goal);
     }
 }
