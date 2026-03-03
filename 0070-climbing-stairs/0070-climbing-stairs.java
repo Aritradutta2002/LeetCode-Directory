b0b60dba0a1857fc6fc63ca0398b4ad640ed37c4
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-04 02:56:46 +0530

    0 ms (100%) 42.2 MB (34.18%) 03-04-2026 0070-climbing-stairs Easy .java

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,15 +2,15 @@ class Solution {
     public int climbStairs(int n) {
         if(n == 1 || n == 2) return n;
 
-        int prev1 = 1;
-        int prev2 = 2;
+        int first = 1;
+        int second = 2;
         int curr = 0;
 
 
         for(int i = 3; i <= n; i++){
-            curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+            curr = first + second;
+            first = second;
+            second = curr;
         }
 
         return curr;
