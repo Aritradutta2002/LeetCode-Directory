e27e8b768ca43de4ff59b7fd03fe28111f35f6a2
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-03 18:53:09 +0530

    N/A (0%) N/A (0%) 03-03-2026 0020-valid-parentheses Easy .java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c == '(' || c == '{' || c == '['){
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
                 st.push(c);
-            }
-            else if(c == ')' || c == '}' || c == ']'){
-                if(st.isEmpty()){
+            } else {
+                if (st.isEmpty()) {
                     return false;
                 }
                 char top = st.pop();
-                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[') ){
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
                     return false;
                 }
             }
