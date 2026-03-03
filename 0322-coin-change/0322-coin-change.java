2b287b59189f9dc315db0281c5dce1a90a79eabc
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-04 00:31:21 +0530

    17 ms (48.9%) 46.5 MB (50.6%) 03-04-2026 0322-coin-change Medium .java

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,7 +2,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
+        Arrays.fill(dp, 10005);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
