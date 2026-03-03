220e1b1f94da0b6a2e1658a6b87d2b840a4bdc57
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-03 10:54:09 +0530

    51 ms (31.49%) 62.4 MB (9.49%) 03-03-2026 1545-find-kth-bit-in-nth-binary-string Medium .java

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        String[] s = new String[n];
+        s[0] = "0";
+
+        for (int i = 1; i < n; i++) {
+            s[i] = s[i - 1] + "1" + reverse(invert(s[i - 1]));
+        }
+
+        return s[n - 1].charAt(k - 1);
+
+    }
+
+    public static String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public static String invert(String s) {
+        char[] c = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            c[i] = s.charAt(i) == '0' ? '1' : '0';
+        }
+        return new String(c);
+    }
+
+}
