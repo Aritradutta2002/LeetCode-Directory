9cab45fc00cdf6cd45f90115599e3ff2a7676ea5
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-03 18:50:49 +0530

    4 ms (42.12%) 43.1 MB (86.34%) 03-03-2026 0020-valid-parentheses Easy .java

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,20 @@
 class Solution {
-   public static boolean isValid(String s){
-        Stack <Character> st = new Stack<>();
-        for(var ch : s.toCharArray()){
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
             }
-            else{
-
-                if(st.isEmpty() || !isMatch(st.pop(), ch)) {
-                return false;}
+            else if(c == ')' || c == '}' || c == ']'){
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top = st.pop();
+                if((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[') ){
+                    return false;
+                }
             }
         }
         return st.isEmpty();
-    }
-
-    private static boolean isMatch(char open, char close ){
-        return (open == '(' && close == ')') ||
-                (open == '{' && close == '}') ||
-                (open == '[' && close == ']');
     }
 }
