0bc718f843ceba4f7122c5c500d40cc6d1e504b1
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-05 11:00:59 +0530

    5 ms (53.53%) 43.8 MB (63.86%) 03-05-2026 1758-minimum-changes-to-make-alternating-binary-string Easy .java

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,13 +1,24 @@
 class Solution {
     public int minOperations(String s) {
-        int n = s.length();
-        int cost0 = 0; 
-
-        for (int i = 0; i < n; i++) {
-            char expected = (char) ('0' + (i % 2));
-            if (s.charAt(i) != expected)
-                cost0++;
+        int start0 = 0;
+        int start1 = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '0') {
+                    start1++;
+                } else {
+                    start0++;
+                }
+            } else {
+                if (s.charAt(i) == '1') {
+                    start1++;
+                } else {
+                    start0++;
+                }
+            }
         }
-        return Math.min(cost0, n - cost0);
+        
+        return Math.min(start0, start1);
     }
 }
