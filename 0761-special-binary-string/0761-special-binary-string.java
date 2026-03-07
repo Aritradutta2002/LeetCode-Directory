50f6b812d614ab9a2c8cbb7acac9d2fbfbd1a82f
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-07 23:45:56 +0530

    3 ms (68.53%) 43.1 MB (82.84%) 03-07-2026 0761-special-binary-string Hard .java

---
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> specials = new ArrayList<>();
+        if (s.length() <= 2)
+            return s;
+        int height = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            height += (s.charAt(i) == '1') ? 1 : -1;
+            if (height == 0) {
+                // Recurse on inner part: s[start+1 .. i-1]
+                String inner = makeLargestSpecial(s.substring(start + 1, i));
+                specials.add("1" + inner + "0");
+                start = i + 1;
+            }
+        }
+
+        Collections.sort(specials, Collections.reverseOrder());
+        return String.join("", specials);
+    }
+}
