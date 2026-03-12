c80b7286871a1d74ba2df4f65dd865672bf64dc4
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-12 22:53:52 +0530

    0 ms (100%) 42.9 MB (32.68%) 03-12-2026 0198-house-robber Medium .java

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int prev2 = 0;
-        int prev1 = nums[0];
+        int n = nums.length;
+        int[] dp = new int[n];
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        for (int i = 1; i < nums.length; i++) {
-            int rob = nums[i] + prev2;
-            int skip = prev1;
-            int curr = Math.max(rob, skip);
-            prev2 = prev1;
-            prev1 = curr;
+        for(int i = 2; i < n; i++){
+            int option1 = dp[i - 2] + nums[i];
+            int option2 = dp[i - 1];
+            dp[i] = Math.max(option1, option2);
         }
 
-        return prev1;
+        return dp[n - 1];
     }
 }
