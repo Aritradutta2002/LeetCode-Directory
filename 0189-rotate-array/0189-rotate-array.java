7b5044aa00e9f0260853d204f46e59bbd257aa9e
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-15 19:59:39 +0530

    1 ms (43.43%) 63.5 MB (7.33%) 03-15-2026 0189-rotate-array Medium .java

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,15 @@
 class Solution {
-    static public void rotate(int[] nums, int k) {
+    public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
+        int[] ans = new int[n];
 
-        reverseArray(nums, 0, n - 1);
-        reverseArray(nums, 0, k - 1);
-        reverseArray(nums, k, n - 1);
-    }
+        for (int i = 0; i < n; i++) {
+            ans[(i + k) % n] = nums[i];
+        }
 
-    static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    static void reverseArray(int[] nums, int i, int j) {
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
-            j--;
+        for (int i = 0; i < n; i++) {
+            nums[i] = ans[i];
         }
     }
 }
