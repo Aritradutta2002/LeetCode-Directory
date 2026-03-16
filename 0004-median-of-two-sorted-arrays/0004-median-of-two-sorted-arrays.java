37fa3f81cdca5b9d3fd3a4a2658aff3bb0c3cc4e
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-16 11:35:47 +0530

    2 ms (47.88%) 49.1 MB (28.12%) 03-16-2026 0004-median-of-two-sorted-arrays Hard .java

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,39 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       int n1 = nums1.length, n2 = nums2.length;
+        List<Integer> list = new ArrayList<>();
+        int l1 = 0;
+        int l2 = 0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
-
-        int n = n1 + n2;
-        
-     
-        int left = (n1 + n2 + 1) / 2; 
-
-       
-        int low = 0, high = n1;
-        while (low <= high) {
-            
-            int mid1 = (low + high) >>> 1; 
-            
-         
-            int mid2 = left - mid1; 
-
-           
-            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
-            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
-            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
-            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
-
-            if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) return Math.max(l1, l2);
-                else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-            } 
-            else if (l1 > r2) {
-                high = mid1 - 1;
-            } else {
-                low = mid1 + 1;
+        while(l1 < n1 && l2 < n2){
+            if(nums1[l1] < nums2[l2]){
+                list.add(nums1[l1]);
+                l1++;
+            }else{
+                list.add(nums2[l2]);
+                l2++;
             }
         }
-        return 0; 
+
+        while(l1 < n1){
+            list.add(nums1[l1++]);
+        }
+
+        while(l2 < n2){
+            list.add(nums2[l2++]);
+        }
+        int mid = list.size() / 2;
+        if((list.size() & 1) != 0){
+            return (double) list.get(mid);
+        } else{
+            return (double) (list.get(mid - 1) + list.get(mid)) / 2;
+        }
     }
 }
