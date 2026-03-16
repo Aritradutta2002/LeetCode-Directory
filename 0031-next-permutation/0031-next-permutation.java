cd2fecc806628c08a54fe552baadafc37d383a7f
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-17 02:21:56 +0530

    0 ms (100%) 44.6 MB (64.75%) 03-17-2026 0031-next-permutation Medium .java

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,25 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int index = -1;
-        int n = nums.length;
-
-        // step - 1 : finding the breakpoint where nums[i + 1] > nums[i]
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i + 1] > nums[i]) {
-                index = i;
-                break;
-            }
+        int n = nums.length, i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1])
+            i--;
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i])
+                j--;
+            swap(nums, i, j);
         }
-
-        // step - 2 : finding the just greater than the breakpoint indexed number
-        if (index != -1) {
-            for (int i = n - 1; i > index; i--) {
-                if (nums[i] > nums[index]) {
-                    swap(nums, i, index);
-                    break;
-                }
-            }
-        }
-
-        // step - 3 : Reverse the array from index + 1 to n - 1
-        reverseArray(nums, index + 1, n - 1);
+        reverse(nums, i + 1, n - 1);
     }
 
-// Implementations of the Inner Methods -  (Nothing else very easy)
-    public void swap(int[] arr, int x, int y) {
-        int temp = arr[x];
-        arr[x] = arr[y];
-        arr[y] = temp;
+    static void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
     }
 
-    public void reverseArray(int[] nums, int start, int end) {
-        while (end > start) {
-            swap(nums, start++, end--);
-        }
+    static void reverse(int[] a, int l, int r) {
+        while (l < r)
+            swap(a, l++, r--);
     }
 }
