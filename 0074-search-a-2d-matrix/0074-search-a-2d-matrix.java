3b0a635bd342486202bdee19d79b16867ad2b54a
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-18 12:54:26 +0530

    N/A (0%) N/A (0%) 03-18-2026 0074-search-a-2d-matrix Medium .java

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -6,15 +6,12 @@ class Solution {
         int low = 0;
         int high = row * col - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            int midValue = matrix[mid / col][mid % col];
-            if (midValue == target)
-                return true;
-            else if (midValue > target)
-                high = mid - 1;
-            else
-                low = mid + 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int midValue = matrix[mid/col][mid % ];
+            if(midValue == target) return true;
+            else if(midValue > target) high = mid - 1;
+            else low = mid + 1;
         }
 
         return false;
