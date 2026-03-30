a161011ab1ac018720c3cc4612b4c836a36b0b13
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-03-30 15:21:27 +0530

    N/A (0%) N/A (0%) 03-30-2026 3546-equal-sum-grid-partition-i Medium .java

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -4,12 +4,11 @@ class Solution {
         long[] rowPS = new long[n + 1];
         long[] colPS = new long[m + 1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++) {
                 rowPS[i + 1] += grid[i][j];
                 colPS[j + 1] += grid[i][j];
             }
-        }
 
         for (int i = 1; i <= n; i++)
             rowPS[i] += rowPS[i - 1];
@@ -21,12 +20,10 @@ class Solution {
             return false;
         long half = total / 2;
 
-        for (int i = 1; i < n; i++)
-            if (rowPS[i] == half)
-                return true;
-        for (int j = 1; j < m; j++)
-            if (colPS[j] == half)
-                return true;
+        if (Arrays.binarySearch(rowPS, 1, n, half) >= 0)
+            return true;
+        if (Arrays.binarySearch(colPS, 1, m, half) >= 0)
+            return true;
 
         return false;
     }
