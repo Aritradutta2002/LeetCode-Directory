c1e450cbb779a9fcb5a07e45dca5f33d88a5b470
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-03 11:24:42 +0530

    N/A (0%) N/A (0%) 04-03-2026 0342-power-of-four Easy .java

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        // A power of four must be a positive number.
-        if (n <= 0) {
+        if (n == 1)
+            return true;
+        if (n <= 0)
             return false;
+        if (n > 0) {
+            while (n % 4 == 0) {
+                n = n / 4;
+            }
         }
-
-        // Repeatedly divide by 4.
-        while (n % 4 == 0) {
-            n /= 4;
-        }
-
-        // If the number is a power of four, we should be left with 1.
         return n == 1;
+
     }
 }
