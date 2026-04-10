230c2faa362179857f78dedbf6067203f5733cb4
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-10 12:16:18 +0530

    1 ms (100%) 44.5 MB (59.16%) 04-10-2026 3740-minimum-distance-between-three-equal-elements-i Easy .java

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -1,23 +1,28 @@
 class Solution {
-
     public int minimumDistance(int[] nums) {
-        int n = nums.length;
-        int ans = n + 1;
-
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                if (nums[i] != nums[j]) {
-                    continue;
-                }
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[j] == nums[k]) {
-                        ans = Math.min(ans, k - i);
-                        break;
-                    }
-                }
-            }
+        int maxVal = 0;
+        for (int num : nums) {
+            if (num > maxVal)
+                maxVal = num;
         }
 
-        return ans == n + 1 ? -1 : ans * 2;
+        int[] last = new int[maxVal + 1];
+        int[] secondLast = new int[maxVal + 1];
+        Arrays.fill(last, -1);
+        Arrays.fill(secondLast, -1);
+
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (secondLast[x] != -1) {
+                int dist = 2 * (i - secondLast[x]);
+                minDistance = Math.min(minDistance, dist);
+            }
+            secondLast[x] = last[x];
+            last[x] = i;
+        }
+
+        return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 }
