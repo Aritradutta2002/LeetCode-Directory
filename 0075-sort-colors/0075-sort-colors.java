e9786e3c4e7606d03bcf862bb142d0d3a836dec0
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-18 02:24:56 +0530

    N/A (0%) N/A (0%) 04-18-2026 0075-sort-colors Medium .java

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,5 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int countZero = 0;
-        int countOne = 0;
-        int countTwo = 0;
-        int j = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                countZero++;
-            }
-            if(nums[i] == 1){
-                countOne++;
-            }
-            if(nums[i] == 2){
-                countTwo++;
-            }
-        }
-
-        while(countZero-->0){
-            nums[j++] = 0;
-        }
-
-        while(countOne-->0){
-            nums[j++] = 1;
-        }
-
-        while(countTwo-->0){
-            nums[j++] = 2;
-        }
+        Arrays.sort(nums);
     }
 }
