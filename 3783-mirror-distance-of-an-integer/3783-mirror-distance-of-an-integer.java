d869b75e18899babdbb83be8768e540a4552db69
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-18 11:43:54 +0530

    N/A (0%) N/A (0%) 04-18-2026 3783-mirror-distance-of-an-integer Easy .java

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
@@ -1,14 +1,8 @@
 class Solution {
     public int mirrorDistance(int n) {
-        return Math.abs(n - reverse(n));
-    }
-
-    public int reverse(int x){
-        int y = 0;
-        while(x > 0){
-            y = y * 10 + (x % 10);
-            x = x / 10;
-        }
-        return y;
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder(s);
+        int reverse = Integer.valueOf(sb.reverse().toString());
+        return Math.abs(n - reverse);
     }
 }
