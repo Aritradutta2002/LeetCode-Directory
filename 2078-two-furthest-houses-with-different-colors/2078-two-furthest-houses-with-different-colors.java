d5b0facd37f4ddac7067dd3b42e1158043f8bb3a
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-20 18:13:53 +0530

    3 ms (9.23%) 43.5 MB (27.78%) 04-20-2026 2078-two-furthest-houses-with-different-colors Easy .java

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -1,15 +1,25 @@
 class Solution {
     public int maxDistance(int[] colors) {
-        int n = colors.length;
-        int maxDistance = 0;
+        Map<Integer, Integer> first = new HashMap<>();
+        Map<Integer, Integer> last = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (colors[i] != colors[j]) {
-                    maxDistance = Math.max(maxDistance, j - i);
+        for (int i = 0; i < colors.length; i++) {
+            int color = colors[i];
+            if (!first.containsKey(color)) {
+                first.put(color, i);
+            }
+            last.put(color, i);
+        }
+
+        int maxDist = 0;
+        for (int color1 : first.keySet()) {
+            for (int color2 : last.keySet()) {
+                if (color1 != color2) {
+                    maxDist = Math.max(maxDist, Math.abs(last.get(color2) - first.get(color1)));
                 }
             }
         }
-        return maxDistance;
+
+        return maxDist;
     }
 }
