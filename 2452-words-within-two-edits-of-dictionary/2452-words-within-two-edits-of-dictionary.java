5dfdd243023c84cc7b99acdd01e0b5d16eb9ebf6
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-22 17:47:41 +0530

    N/A (0%) N/A (0%) 04-22-2026 2452-words-within-two-edits-of-dictionary Medium .java

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -2,8 +2,8 @@ class Solution {
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
         List<String> ans = new ArrayList<>();
         for (int i = 0; i < queries.length; i++) {
-            for(int j = 0; j < dictionary.length; j++){
-                if(isWithinTwoEdits(queries[i], dictionary[j])){
+            for (int j = 0; j < dictionary.length; j++) {
+                if (isWithinTwoEdits(queries[i], dictionary[j])) {
                     ans.add(queries[i]);
                     break;
                 }
