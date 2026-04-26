92d2a86bb258b7a4d8e6b192fda76713ac06b680
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-04-26 10:25:50 +0530

    N/A (0%) N/A (0%) 04-26-2026 1559-detect-cycles-in-2d-grid Medium .java

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,16 +1,16 @@
 class Solution {
-    private int[] dx = {-1, 1, 0, 0};
-    private int[] dy = {0, 0, -1, 1};
+    private int[] dx = { -1, 1, 0, 0 };
+    private int[] dy = { 0, 0, -1, 1 };
 
     public boolean containsCycle(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int count = 0;
         boolean[][] visited = new boolean[n][m];
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (!visited[i][j]) {
-                    if (dfs(i, j, -1, -1, grid, visited)) {
+                    if (bfs(i, j, -1, -1, grid, visited)) {
                         return true;
                     }
                 }
@@ -19,17 +19,35 @@ class Solution {
         return false;
     }
 
-    private boolean dfs(int x, int y, int px, int py, char[][] grid, boolean[][] visited) {
-        visited[x][y] = true;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == grid[x][y]) {
-                if (!visited[nx][ny]) {
-                    if (dfs(nx, ny, x, y, grid, visited)) {
-                        return true;
-                    }
-                } else if (nx != px || ny != py) {
+    public boolean bfs(int rr, int cc, int px, int py, char[][] grid, boolean[][] visited) {
+        Queue<int[]> child = new ArrayDeque<>();
+        Queue<int[]> parent = new ArrayDeque<>();
+        int n = grid.length;
+        int m = grid[0].length;
+        visited[rr][cc] = true;
+        child.offer(new int[] { rr, cc });
+        parent.offer(new int[] { px, py });
+
+        while (!child.isEmpty()) {
+            int[] c = child.poll();
+            int[] p = parent.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + c[0];
+                int ny = dy[i] + c[1];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m
+                        && grid[nx][ny] == grid[c[0]][c[1]]
+                        && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    child.offer(new int[] { nx, ny });
+                    parent.offer(new int[] { c[0], c[1] });
+                }
+
+                else if (nx >= 0 && nx < n && ny >= 0 && ny < m
+                        && visited[nx][ny]
+                        && grid[nx][ny] == grid[c[0]][c[1]]
+                        && (nx != p[0] || ny != p[1])) {
                     return true;
                 }
             }
