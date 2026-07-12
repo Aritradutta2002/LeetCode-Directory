c08128fa0b64745f996a5b30bc3d7aac71757b41
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-07-12 12:55:48 +0530

    36 ms (32.64%) 70.5 MB (92.98%) 07-12-2026 1331-rank-transform-of-an-array Easy .java

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -13,23 +13,8 @@ class Solution {
         }
         Arrays.sort(temp);
         for (int i = 0; i < n; i++) {
-            ans[i] = binarySearch(arr[i], temp);
+            ans[i] = Arrays.binarySearch(temp, arr[i]) + 1;
         }
         return ans;
-    }
-
-    public int binarySearch(int number, int[] arr) {
-        int low = 0;
-        int high = arr.length-1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (number == arr[mid])
-                return mid+1;
-            if (number > arr[mid])
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        return -1;
     }
 }
