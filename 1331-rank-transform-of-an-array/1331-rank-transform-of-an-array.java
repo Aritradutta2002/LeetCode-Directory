f916c6f4b88abac8031a12a92ed1cf344565c2f9
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-07-12 13:13:03 +0530

    27 ms (98.42%) 77.2 MB (20.51%) 07-12-2026 1331-rank-transform-of-an-array Easy .java

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -2,18 +2,17 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
         int n = arr.length;
         int[] ans = new int[n];
-        HashSet<Integer> set = new HashSet<>();
-        for (int i : arr) {
-            set.add(i);
-        }
-        int[] temp = new int[set.size()];
-        int idx = 0;
-        for (int i : set) {
-            temp[idx++] = i;
-        }
-        Arrays.sort(temp);
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        int r = 0;
         for (int i = 0; i < n; i++) {
-            ans[i] = Arrays.binarySearch(temp, arr[i]) + 1;
+            if (i == 0 || sorted[i] != sorted[i - 1]) {
+                rank.put(sorted[i], ++r);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            ans[i] = rank.get(arr[i]);
         }
         return ans;
     }
