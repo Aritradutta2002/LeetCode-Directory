d73f40dc89ce31ca1ea2d94c7d46cf54ca53a744
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-07-17 01:54:00 +0530

    1 ms (81.01%) 42.5 MB (73.57%) 07-17-2026 3658-gcd-of-odd-and-even-sums Easy .java

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,32 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int oddSum = 0;
-        int evenSum = 0;
-        int oddCount = 0;
-        int evenCount = 0;
-
-        for (int i = 1; i <= 2000; i++) {
-            if (i % 2 == 0) {
-                evenSum += i;
-                evenCount++;
-            }
-            if (evenCount == n)
-                break;
-        }
-
-        for (int i = 1; i <= 2000; i++) {
-            if (i % 2 != 0) {
-                oddSum += i;
-                oddCount++;
-            }
-            if (oddCount == n)
-                break;
-        }
-
-        return gcd(oddCount, evenCount);
-    }
-
-    static int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        return n;
     }
 }
