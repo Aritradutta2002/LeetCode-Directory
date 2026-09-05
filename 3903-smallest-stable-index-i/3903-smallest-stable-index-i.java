51d55b3606da2165db787a66a4c4b7b7d3a0765f
ARITRA DUTTA <131231940+Aritradutta2002@users.noreply.github.com>
2026-09-05 11:42:36 +0530

    1 ms (99.79%) 46.2 MB (88.21%) 09-05-2026 3903-smallest-stable-index-i Easy .java

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,26 +1,23 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-        int[] suffixMin = new int[n];
-        suffixMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
-        }
-
-        int prefixMax = nums[0];
         for (int i = 0; i < n; i++) {
-            // instability = max(nums[0..i]) - min(nums[i..n‑1])
-            int instability = prefixMax - suffixMin[i];
-            if (instability <= k) {
+            int maxValue = nums[i];
+            int minValue = nums[i];
+
+            for (int j = 0; j < i; j++) {
+                maxValue = Math.max(nums[j], maxValue);
+            }
+
+            for (int j = i + 1; j < n; j++) {
+                minValue = Math.min(nums[j], minValue);
+            }
+
+            if (maxValue - minValue <= k) {
                 return i;
             }
-
-            // update prefix max for the next iteration
-            if (i + 1 < n && nums[i + 1] > prefixMax) {
-                prefixMax = nums[i + 1];
-            }
         }
 
-        return -1; // no 
+        return -1;
     }
 }
